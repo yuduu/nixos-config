aaa6f7c2fed82028c018b6ac1fa0ba78df201aa2
Yuduu <yuduu@bravecatcastle.de>
2025-12-26 11:01:16 +0100

    Add Heroic Games Launcher to yuduu-desktop

--- hosts/yuduu-desktop/default.nix
+++ hosts/yuduu-desktop/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
@@ -7,6 +7,24 @@
   ];
 
   networking.hostName = "yuduu-desktop";
+
+  # Gaming-specific configuration for this host.
+  boot.kernelModules = [ "xpad" ];
+
+  hardware.graphics.enable32Bit = true;
+  hardware.steam-hardware.enable = true;
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+    extraCompatPackages = with pkgs; [ proton-ge-bin ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    heroic
+  ];
 
   system.stateVersion = "25.05";
 }
